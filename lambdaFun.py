72cdef970c723135a0697ac43602eb361d5748d5
Ishaan Bhela <79138066+Ishaan453@users.noreply.github.com>
2024-03-06 19:54:54 +0530

    Update lambdaFun.py

--- lambdaFun.py
+++ lambdaFun.py
@@ -1,22 +1,26 @@
 import boto3
+import string
+import random
 import json
 
 def lambda_handler(event, context):
     text = event['text']
+    speaker = event['speaker']
 
     polly = boto3.client('polly')
     s3 = boto3.client('s3')
-
+    
+    random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
+    MP3_Name = f"{random_string}.mp3"
+    print(MP3_Name)
     try:
         # Synthesize speech using Polly
-        response = polly.synthesize_speech(Text=text, OutputFormat='mp3', VoiceId='Joanna')
-        print(text)
-        print("Response:", response['AudioStream'])
+        response = polly.synthesize_speech(Text=text, OutputFormat='mp3', VoiceId=speaker)
 
         # Store audio in a temporary S3 bucket
         s3.put_object(
             Bucket='temp-audio-text-to-speech',
-            Key='speech.mp3', 
+            Key=MP3_Name, 
             Body=response['AudioStream'].read()
         )
 
@@ -24,7 +28,7 @@ def lambda_handler(event, context):
         # Generate a presigned URL for the audio file
         presigned_url = s3.generate_presigned_url(
             ClientMethod='get_object',
-            Params={'Bucket': 'temp-audio-text-to-speech', 'Key': 'speech.mp3'},
+            Params={'Bucket': 'temp-audio-text-to-speech', 'Key': MP3_Name},
             ExpiresIn=3600 
         )
 
